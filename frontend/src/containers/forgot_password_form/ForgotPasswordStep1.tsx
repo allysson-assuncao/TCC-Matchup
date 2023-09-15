eff11fce719f15fa21c5309edc666fb5094f075b
Allysson Bruno Chaves Assunção <0063398@academico.ifmg.edu.br>
2023-09-14 21:30:17 -0300

    finishing the forgot password page (basic structure and style), no validations or requests yet

--- frontend/src/containers/forgot_password_form/ForgotPasswordStep1.tsx
+++ frontend/src/containers/forgot_password_form/ForgotPasswordStep1.tsx
@@ -1,110 +1,25 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import ForgotPassword from "../../pages/forgot_password/ForgotPassword";
+import {Container, Grid, Typography, TextField} from '@mui/material';
+import theme from "../../theme";
 
 const ForgotPasswordStep1: React.FC = () => {
     return (
         <React.Fragment>
-            <Typography variant="h6" gutterBottom>
-                Shipping address
-            </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="firstName"
-                        name="firstName"
-                        label="First name"
-                        fullWidth
-                        autoComplete="given-name"
-                        variant="standard"
-                    />
+            <Container>
+                <Grid justifyContent={"center"}>
+                    <Grid item>
+                        <Grid container justifyContent="center">
+                            <Typography variant={"h5"} color={theme.palette.primary.main} sx={{marginBottom: '16px'}}>Esqueceu
+                                sua senha?</Typography>
+                        </Grid>
+                        <Typography sx={{marginBottom: '16px'}}>Informe o seu email e nós enviaremos um código
+                            para
+                            a redefinição da senha!</Typography>
+                        <TextField color='primary' id="email" label="Seu Email" required autoFocus type="email"
+                                   placeholder="exemplo@gmail.com" fullWidth/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="lastName"
-                        name="lastName"
-                        label="Last name"
-                        fullWidth
-                        autoComplete="family-name"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="address1"
-                        name="address1"
-                        label="Address line 1"
-                        fullWidth
-                        autoComplete="shipping address-line1"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        id="address2"
-                        name="address2"
-                        label="Address line 2"
-                        fullWidth
-                        autoComplete="shipping address-line2"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="city"
-                        name="city"
-                        label="City"
-                        fullWidth
-                        autoComplete="shipping address-level2"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        id="state"
-                        name="state"
-                        label="State/Province/Region"
-                        fullWidth
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="zip"
-                        name="zip"
-                        label="Zip / Postal code"
-                        fullWidth
-                        autoComplete="shipping postal-code"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="country"
-                        name="country"
-                        label="Country"
-                        fullWidth
-                        autoComplete="shipping country"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-                        label="Use this address for payment details"
-                    />
-                </Grid>
-            </Grid>
+            </Container>
         </React.Fragment>
     );
 }
--- frontend/src/pages/forgot_password/ForgotPassword.tsx
+++ frontend/src/pages/forgot_password/ForgotPassword.tsx
@@ -21,7 +21,7 @@ function Copyright() {
     );
 }
 
-const steps = ['Validação do email', 'Código de verificação', 'Redefinição de senha'];
+const steps = ['Validação do email', 'Código de verificação', 'Nova senha'];
 
 const ForgotPassword: React.FC = () => {
     const history = useNavigate();
@@ -71,7 +71,7 @@ const ForgotPassword: React.FC = () => {
             <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
                 <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                     <Typography component="h1" variant="h4" align="center">
-                        Checkout
+                        Redefinição de Senha
                     </Typography>
                     <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
                         {steps.map((label) => (
@@ -83,13 +83,13 @@ const ForgotPassword: React.FC = () => {
                     {activeStep === steps.length ? (
                         <React.Fragment>
                             <Typography variant="h5" gutterBottom>
-                                Enviamos o código por email.
+                                Senha redefinida com sucesso!
                             </Typography>
                         </React.Fragment>
                     ) : (
                         <React.Fragment>
                             {getStepContent(activeStep)}
-                            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                            <Grid sx={{ display: 'flex'}} justifyContent={"space-between"}>
                                 {activeStep === 0 && (
                                     <Button component={RouterLink} to={ROUTE_SIGN_IN} sx={{ mt: 3, ml: 1 }}>
                                         Voltar
@@ -108,7 +108,7 @@ const ForgotPassword: React.FC = () => {
                                 >
                                     {activeStep === steps.length - 1 ? 'Redefinir' : 'Próximo'}
                                 </Button>
-                            </Box>
+                            </Grid>
                         </React.Fragment>
                     )}
                 </Paper>
