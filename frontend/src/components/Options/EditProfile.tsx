0e0c9a72e74799b1697e6b7f21a7a2822b26dda9
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-09-22 15:40:12 -0300

    fixing some profile stuff

---
+++ frontend/src/components/Options/EditProfile.tsx
@@ -0,0 +1,76 @@
+import {NavigateFunction, useNavigate, useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {User} from "../../model/user";
+import {ROUTE_SIGN_IN} from "../../App";
+import AppBarProfile from "../../containers/AppBarProfile";
+import {Avatar, Box, Container, CssBaseline, Typography} from "@mui/material";
+import theme from "../../theme";
+import AppBarGeneral from "../../containers/AppBarGeneral";
+
+const EditProfile = () => {
+    const {usernamePathVariable} = useParams();
+    console.log(usernamePathVariable);
+    const history: NavigateFunction = useNavigate();
+
+    const [editable, setEditability] = useState(false);
+
+    const [loggedUser, setLoggedUser] = useState<User | null>(null);
+    const [image, setImage] = useState(undefined);
+    const [name, setName] = useState(undefined);
+    const [bio, setBio] = useState(undefined);
+
+    useEffect(() => {
+            const userJSON = localStorage.getItem('user');
+            if (!userJSON) {
+                history(ROUTE_SIGN_IN);
+            } else {
+                let user;
+                user = JSON.parse(userJSON);
+                if (usernamePathVariable == JSON.parse(userJSON).username) {
+                    setEditability(true);
+                }else{
+                    setEditability(false);
+                }
+                setLoggedUser(user);
+                setImage(user.profilePicture);
+                setName(user.name);
+                setBio(user.bio);
+            }
+        }, []
+    );
+
+    if (!loggedUser) return null;
+
+    return (
+        <React.Fragment>
+            <AppBarGeneral title={"Settings"}></AppBarGeneral>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '40%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        maxWidth: 550,
+                        minWidth: 450,
+                        width: 'auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        border: `1px solid${theme.palette.primary.main}`,
+                        padding: '40px',
+                        borderRadius: '16px',
+                        backgroundColor: '9c27b0',
+                    }}
+                >
+                    <Typography color={theme.palette.primary.main} variant="h4">{name}</Typography>
+                    <Avatar alt={name} src={image} style={{width: '100px', height: '100px', cursor: 'pointer'}}/>
+                    <Typography color={theme.palette.primary.main} variant="body1" align="left">{bio}</Typography>
+                </Box>
+            </Container>
+        </React.Fragment>
+    );
+}
+
+export default EditProfile;
---
+++ frontend/src/containers/AppBarGeneral.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import {
+    AppBar,
+    Box,
+    Button, Container, CssBaseline,
+    Grid,
+    Link,
+    Toolbar, Typography,
+    useScrollTrigger,
+} from "@mui/material";
+import theme from "../theme";
+import {NavigateFunction, useNavigate} from "react-router-dom";
+import {ROUTE_EDITABLE_PROFILE, ROUTE_HOME, ROUTE_SIGN_IN, ROUTE_SIGN_UP} from "../App";
+import ToggleColorModeButton from "../components/ToggleColorModeButton";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import IconButton from "@mui/material/IconButton";
+import {User} from "../model/user";
+import {getUser} from "../pages/home/Home";
+import {useEffect, useState} from "react";
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material/styles";
+
+interface PropsAppBarProfile {
+    title: string;
+}
+
+var loggedUser: User = getUser();
+
+const AppBarProfile: React.FC<PropsAppBarProfile> = ({ title }) => {
+    const history: NavigateFunction = useNavigate();
+
+
+    return (
+        <Box bgcolor={theme.palette.background.default}>
+            <Container component="main">
+                <CssBaseline/>
+                <AppBar
+                    position="static"
+                    color="default"
+                    elevation={0}
+                    sx={{
+                        border: (theme) => `1px solid ${theme.palette.divider}`,
+                        borderRadius: '50px',
+                        marginTop: '15px',
+                        bgcolor: 'background.default',
+
+                    }}
+                >
+                    <Box>
+                        <Toolbar>
+                            <Grid container spacing={3} alignContent='center'>
+                                <Grid item xs alignItems='left' margin={'auto'}>
+                                    <IconButton color="primary" onClick={() => history(ROUTE_HOME)}>
+                                        <ArrowBackIcon/>
+                                    </IconButton>
+                                </Grid>
+                                <Grid item xs={6} textAlign="center" margin="auto" sx={{fontSize: '20px'}}>
+                                    <Typography color={theme.palette.primary.main} variant="h4">{title}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Toolbar>
+                    </Box>
+                </AppBar>
+            </Container>
+        </Box>
+    );
+}
+
+export default AppBarProfile;
