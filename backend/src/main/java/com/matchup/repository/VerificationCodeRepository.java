c9b1ec7a0a3b9f44b893de47bcad4b605a44e93b
Allysson Bruno Chaves Assunção <0063398@academico.ifmg.edu.br>
2023-09-29 13:31:08 -0300

    some changes required to save the handle the creation of the VerificationCode

--- backend/src/main/java/com/matchup/repository/VerificationCodeRepository.java
+++ backend/src/main/java/com/matchup/repository/VerificationCodeRepository.java
@@ -2,8 +2,6 @@ package com.matchup.repository;
 
 import com.matchup.model.VerificationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
