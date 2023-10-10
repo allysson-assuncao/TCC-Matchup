ee074d9df8c5125dcf68acd1ed7cca3f7ca21634
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-10-10 10:56:11 -0300

    User block operations working properly

---
+++ backend/src/main/java/com/matchup/model/Block.java
@@ -0,0 +1,58 @@
+package com.matchup.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "block", schema = "matchup")
+public class Block {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private User blocker;
+
+    @ManyToOne
+    private User blocked;
+
+    // <editor-fold desc="Constructors">
+    public Block() {
+    }
+
+    public Block(User blocker, User blocked) {
+        this.blocker = blocker;
+        this.blocked = blocked;
+    }
+
+    // </editor-fold>
+
+    // <editor-fold desc="Encapsulation">
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getBlocker() {
+        return blocker;
+    }
+
+    public void setBlocker(User blocker) {
+        this.blocker = blocker;
+    }
+
+    public User getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(User blocked) {
+        this.blocked = blocked;
+    }
+
+    // </editor-fold>
+
+}
