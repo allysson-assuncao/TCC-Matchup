0db1b7c6b7564687b79cd9b14dcd5cb10abf4011
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-10-10 11:10:41 -0300

    Still working with Notification in the frontend

--- frontend/src/components/Contact/Notification.tsx
+++ frontend/src/components/Contact/Notification.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { User } from "../../model/user";
-import { Button, Grid } from "@mui/material";
+import {Button, Grid, Typography} from "@mui/material";
 import ProfilePicture from "../ProfilePicture";
 import {getUser} from "../../pages/home/Home";
+import {useCustomTheme} from "../../CustomThemeContext";
+import getTheme from "../../theme";
 
 export const NOTIFICATION_TYPES = {
     DEFAULT: 1,
     FRIENDSHIP_SOLICITATION: 2
+}
+
+export const SOLICITATION_TEXT = {
+    PENDING: ` te enviou um pedido de amizade`,
+    ACCEPTED: ' aceitou seu pedido de amizade',
+    REFUSED: 'negou seu pedido de amizade'
 }
 
 interface NotificationProps {
@@ -17,11 +25,18 @@ interface NotificationProps {
 }
 
 const Notification: React.FC<NotificationProps> = ({ text, type, sender, date }) => {
+    const {theme: mode} = useCustomTheme();
+    const theme = getTheme(mode);
     return (
-        <Grid container>
+        <Grid bgcolor={theme.palette.secondary.main} container>
             <Grid item>
                 {type === NOTIFICATION_TYPES.FRIENDSHIP_SOLICITATION && sender &&
                     <ProfilePicture id={sender.id} small={true}/>
+                }
+            </Grid>
+            <Grid item>
+                {type === NOTIFICATION_TYPES.FRIENDSHIP_SOLICITATION && !text && sender && //accepted?
+                    <Typography color={theme.palette.primary.main}>${sender.username + SOLICITATION_TEXT.PENDING /*iterator*/}</Typography>
                 }
             </Grid>
         </Grid>
