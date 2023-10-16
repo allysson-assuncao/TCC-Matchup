35082a553250845a7e2c029f4c213268e65ae1b6
allysson723 <0063398@academico.ifmg.edu.br>
2023-10-16 13:23:47 -0300

    creating the FriendDto

---
+++ backend/src/main/java/com/matchup/dto/FriendDto.java
@@ -0,0 +1,23 @@
+package com.matchup.dto;
+
+public class FriendDto {
+    private long id;
+    private long username;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUsername() {
+        return username;
+    }
+
+    public void setUsername(long username) {
+        this.username = username;
+    }
+
+}
