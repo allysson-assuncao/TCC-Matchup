430234d64b6b9495f4994d117cd8f36d3735484d
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-12-15 16:50:41 -0300

    Profile picture being exhibited successfully :)

--- frontend/src/components/hook-form/RHFUpload.js
+++ frontend/src/components/hook-form/RHFUpload.js
@@ -1,45 +1,45 @@
 import PropTypes from 'prop-types';
 // form
-import { useFormContext, Controller } from 'react-hook-form';
+import {useFormContext, Controller} from 'react-hook-form';
 // @mui
-import { FormHelperText } from '@mui/material';
+import {FormHelperText} from '@mui/material';
 //
-import { UploadAvatar } from '../upload';
+import {UploadAvatar} from '../upload';
 
 // ----------------------------------------------------------------------
 
 RHFUploadAvatar.propTypes = {
-  name: PropTypes.string,
+    name: PropTypes.string,
 };
 
 // ----------------------------------------------------------------------
 
-export function RHFUploadAvatar({ name, ...other }) {
-  const { control } = useFormContext();
+export function RHFUploadAvatar({name, ...other}) {
+    const {control} = useFormContext();
 
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, fieldState: { error } }) => (
-        <div>
-          <UploadAvatar
-            accept={{
-              'image/*': [],
-            }}
-            error={!!error}
-            file={field.value}
-            {...other}
-          />
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({field, fieldState: {error}}) => (
+                <div>
+                    <UploadAvatar
+                        accept={{
+                            'image/*': [],
+                        }}
+                        error={!!error}
+                        file={field.value}
+                        {...other}
+                    />
 
-          {!!error && (
-            <FormHelperText error sx={{ px: 2, textAlign: 'center' }}>
-              {error.message}
-            </FormHelperText>
-          )}
-        </div>
-      )}
-    />
-  );
+                    {!!error && (
+                        <FormHelperText error sx={{px: 2, textAlign: 'center'}}>
+                            {error.message}
+                        </FormHelperText>
+                    )}
+                </div>
+            )}
+        />
+    );
 }
 
