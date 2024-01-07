12f69c3da5bfee37645863808b74987db4cdaef7
anybody <0063398@academico.ifmg.edu.br>
2024-01-07 11:38:27 -0300

    terrible but reasonably functional card popup done

--- frontend/src/components/our/Interest/InterestCard.tsx
+++ frontend/src/components/our/Interest/InterestCard.tsx
@@ -14,12 +14,9 @@ import {linkInterestToUser, unlinkInterestToUser} from "../../../api/user_reques
 import {useTheme} from "@mui/material/styles";
 import {useDispatch, useSelector} from "react-redux";
 import {showSnackbar} from "../../../redux/slices/app";
-import {FRIENDSHIP_STATUS} from "../../../model/friendship";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import {PersonRemove, Remove} from "@mui/icons-material";
 import ImageUploader from "../fields/ImageUploader";
 import {updateInterestImagesById} from "../../../api/interest_requests/updateInterestImage";
-
 interface InterestCardProps {
     interest: Interest;
 }
@@ -57,119 +54,167 @@ const InterestCard: React.FC<InterestCardProps> = ({interest}) => {
         updateInterestImagesById(newImages, interest.id, token);
     };
 
+
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <Card
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                sx={{
-                    minHeight: isExpanded ? 'auto' : '200px',
-                    transition: 'min-height 0.3s ease-in-out',
-                }}
-            >
-                <ImageUploader
-                    setImages={setNewImages}
-                    handleSave={handleSave}
-                    calledByInterestCard
-                    interestImageList={interest.formattedImages}
-                    userAccess={user.access}/>
-                <CardHeader
-                    title={interest.name}
-                    subheader={interest.company?.name}
-                    titleTypographyProps={{align: 'center'}}
-                    subheaderTypographyProps={{align: 'center'}}
-
+            {isExpanded ? (
+                <Card
+                    onMouseLeave={handleMouseLeave}
                     sx={{
-                        color: (theme) => theme.palette.primary.main,
-                        backgroundColor: (theme) => grey[900],
-                        mb: "5px"
+                        width: '17.5%',
+                        position: 'absolute',
+                        zIndex: theme.zIndex.modal,
+                        minHeight: isExpanded ? 'auto' : '200px',
+                        transition: 'min-height 0.3s ease-in-out',
+                        overflow: isExpanded ? 'visible' : 'hidden',
                     }}
-                />
-                <CardActions sx={{paddingY: "5px"}}>
-                    <Button
-                        onClick={() => {
-                            interest.added ? handleRemoveInterest() : handleAddInterest();
-                            interest.added = !interest.added;
+                >
+                    <ImageUploader
+                        setImages={setNewImages}
+                        handleSave={handleSave}
+                        calledByInterestCard
+                        interestImageList={interest.formattedImages}
+                        userAccess={user.access}/>
+                    <CardHeader
+                        title={interest.name}
+                        subheader={interest.company?.name}
+                        titleTypographyProps={{align: 'center'}}
+                        subheaderTypographyProps={{align: 'center'}}
+
+                        sx={{
+                            color: (theme) => theme.palette.primary.main,
+                            backgroundColor: (theme) => grey[900],
+                            mb: "5px"
                         }}
-                        fullWidth
-                        title={interest.added ? "Remover Interesse" : "Adicionar Interesse"}
-                        startIcon={interest.added ? <Remove/> : <AddIcon/>}
-                        variant="outlined"
-                    >
-                        {interest.added ? "Remover" : "Adicionar"}
-                    </Button>
-                </CardActions>
-                {isExpanded && (
-                    <CardContent sx={{padding: "0px", marginX: "15px"}}>
-                        <Typography variant="body2" color="text.secondary">
-                            Descrição: {interest.description}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Preço: R${interest.lowestPrice} ~ R${interest.highestPrice}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Idade: {interest.ageRating?.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Gêneros:
-                        </Typography>
-                        <Stack style={{display: 'flex', overflowX: 'auto'}}>
+                    />
+                    <CardActions sx={{paddingY: "5px"}}>
+                        <Button
+                            onClick={() => {
+                                interest.added ? handleRemoveInterest() : handleAddInterest();
+                                interest.added = !interest.added;
+                            }}
+                            fullWidth
+                            title={interest.added ? "Remover Interesse" : "Adicionar Interesse"}
+                            startIcon={interest.added ? <Remove/> : <AddIcon/>}
+                            variant="outlined"
+                        >
+                            {interest.added ? "Remover" : "Adicionar"}
+                        </Button>
+                    </CardActions>
+                    {isExpanded && (
+                        <CardContent sx={{padding: "0px", marginX: "15px"}}>
+                            <Typography variant="body2" color="text.secondary">
+                                Descrição: {interest.description}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Preço: R${interest.lowestPrice} ~ R${interest.highestPrice}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Idade: {interest.ageRating?.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Gêneros:
+                            </Typography>
+                            <Stack style={{display: 'flex', overflowX: 'auto'}}>
+                                {interest && interest.genres.length != 0
+                                    && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
+                                        {interest.genres.map((genre, index) => (
+                                            <Chip key={index} label={genre.name + ""} style={{margin: 0}}
+                                                  sx={{backgroundColor: "#7635dc"}}/>
+                                        ))}
+                                    </Stack>)
+                                }
+                            </Stack>
+                            <Typography variant="body2" color="text.secondary">
+                                Subgêneros:
+                            </Typography>
                             {interest && interest.genres.length != 0
                                 && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
-                                    {interest.genres.map((genre, index) => (
-                                        <Chip key={index} label={genre.name + ""} style={{margin: 0}}
-                                              sx={{backgroundColor: "#7635dc"}}/>
+                                    {interest.subGenres.map((subGenre, index) => (
+                                        <Chip key={index} label={subGenre.name + ""} style={{margin: 4}}
+                                              sx={{backgroundColor: "#0162c4"}}/>
                                     ))}
                                 </Stack>)
                             }
-                        </Stack>
-                        <Typography variant="body2" color="text.secondary">
-                            Subgêneros:
-                        </Typography>
-                        {interest && interest.genres.length != 0
-                            && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
-                                {interest.subGenres.map((subGenre, index) => (
-                                    <Chip key={index} label={subGenre.name + ""} style={{margin: 4}}
-                                          sx={{backgroundColor: "#0162c4"}}/>
-                                ))}
-                            </Stack>)
-                        }
-                        <Typography variant="body2" color="text.secondary">
-                            Plataformas:
-                        </Typography>
-                        {interest && interest.genres.length != 0
-                            && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
-                                {interest.platforms.map((platform, index) => (
-                                    <Chip key={index} label={platform.name + ""} style={{margin: 4}}
-                                          sx={{backgroundColor: "#fda92d"}}/>
-                                ))}
-                            </Stack>)
-                        }
-                        <Typography variant="body2" color="text.secondary">
-                            Dublado:
-                        </Typography>
-                        {interest && interest.genres.length != 0
-                            && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
-                                {interest.dubbingLanguages.map((dubbingLanguage, index) => (
-                                    <Chip key={index} label={dubbingLanguage.id + ""} style={{margin: 4}}
-                                          sx={{backgroundColor: "#1ccaff"}}/>
-                                ))}
-                            </Stack>)
-                        }
-                        <Typography variant="body2" color="text.secondary">
-                            Legendado:
-                        </Typography>
-                        {interest && interest.genres.length != 0
-                            && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
-                                {interest.subtitleLanguages.map((subtitleLanguage, index) => (
-                                    <Chip key={index} label={subtitleLanguage.id + ""} style={{margin: 4}}
-                                          sx={{backgroundColor: "#ff3030"}}/>
-                                ))}
-                            </Stack>)
-                        }
-                    </CardContent>
-                )}
-            </Card>
+                            <Typography variant="body2" color="text.secondary">
+                                Plataformas:
+                            </Typography>
+                            {interest && interest.genres.length != 0
+                                && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
+                                    {interest.platforms.map((platform, index) => (
+                                        <Chip key={index} label={platform.name + ""} style={{margin: 4}}
+                                              sx={{backgroundColor: "#fda92d"}}/>
+                                    ))}
+                                </Stack>)
+                            }
+                            <Typography variant="body2" color="text.secondary">
+                                Dublado:
+                            </Typography>
+                            {interest && interest.genres.length != 0
+                                && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
+                                    {interest.dubbingLanguages.map((dubbingLanguage, index) => (
+                                        <Chip key={index} label={dubbingLanguage.id + ""} style={{margin: 4}}
+                                              sx={{backgroundColor: "#1ccaff"}}/>
+                                    ))}
+                                </Stack>)
+                            }
+                            <Typography variant="body2" color="text.secondary">
+                                Legendado:
+                            </Typography>
+                            {interest && interest.genres.length != 0
+                                && (<Stack direction={'row'} justifyContent={"start"} spacing={5}>
+                                    {interest.subtitleLanguages.map((subtitleLanguage, index) => (
+                                        <Chip key={index} label={subtitleLanguage.id + ""} style={{margin: 4}}
+                                              sx={{backgroundColor: "#ff3030"}}/>
+                                    ))}
+                                </Stack>)
+                            }
+                        </CardContent>
+                    )}
+                </Card>
+            ) : (
+                <Card
+                    onMouseEnter={handleMouseEnter}
+                    sx={{
+                        width: '100%',
+                        minHeight: isExpanded ? 'auto' : '200px',
+                        transition: 'min-height 0.3s ease-in-out',
+                    }}
+                >
+                    <ImageUploader
+                        setImages={setNewImages}
+                        handleSave={handleSave}
+                        calledByInterestCard
+                        interestImageList={interest.formattedImages}
+                        userAccess={user.access}/>
+                    <CardHeader
+                        title={interest.name}
+                        subheader={interest.company?.name}
+                        titleTypographyProps={{align: 'center'}}
+                        subheaderTypographyProps={{align: 'center'}}
+
+                        sx={{
+                            color: (theme) => theme.palette.primary.main,
+                            backgroundColor: (theme) => grey[900],
+                            mb: "5px"
+                        }}
+                    />
+                    <CardActions sx={{paddingY: "5px"}}>
+                        <Button
+                            onClick={() => {
+                                interest.added ? handleRemoveInterest() : handleAddInterest();
+                                interest.added = !interest.added;
+                            }}
+                            fullWidth
+                            title={interest.added ? "Remover Interesse" : "Adicionar Interesse"}
+                            startIcon={interest.added ? <Remove/> : <AddIcon/>}
+                            variant="outlined"
+                        >
+                            {interest.added ? "Remover" : "Adicionar"}
+                        </Button>
+                    </CardActions>
+                </Card>
+            )}
         </Grid>
     );
 };
